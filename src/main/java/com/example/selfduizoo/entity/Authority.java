fd95f0fe7173153fded300d59b16cac7284bfb2c
“mafatofu” <qwqw0310@gmail.com>
2024-06-11 09:17:43 +0900

    관리자 권한 추가

--- src/main/java/com/example/selfduizoo/entity/Authority.java
+++ src/main/java/com/example/selfduizoo/entity/Authority.java
@@ -6,7 +6,8 @@ import lombok.Getter;
 public enum Authority {
     ROLE_DORMANT_USER("휴면사용자"),
     ROLE_INACTIVE_USER("비활성사용자"),
-    ROLE_USER("일반사용자");
+    ROLE_USER("일반사용자"),
+    ROLE_ADMIN("관리자");
 
     private final String authority;
     Authority(String authority) {
